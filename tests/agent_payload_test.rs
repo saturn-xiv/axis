24711464d99f707b3b34e246956d6dea2bbc61c8
Jeremy Zheng <jitang.zheng@gmail.com>
2019-02-09 16:56:55 -0800

    :bug: fix test payload

--- tests/agent_payload_test.rs
+++ tests/agent_payload_test.rs
@@ -5,12 +5,30 @@ use axis::agent::task::Payload;
 #[test]
 fn it_shell() {
     for it in vec![
-        Payload::Shell(("root".to_string(), "uname -a".to_string())),
-        Payload::Shell(("root".to_string(), r#"echo "$PATH""#.to_string())),
-        Payload::Shell(("root".to_string(), "whoami".to_string())),
-        Payload::Shell(("jeremy".to_string(), "date".to_string())),
-        Payload::Shell(("jeremy".to_string(), r#"echo "$PATH""#.to_string())),
-        Payload::Shell(("jeremy".to_string(), "whoami".to_string())),
+        Payload::Shell {
+            user: "root".to_string(),
+            script: "uname -a".to_string(),
+        },
+        Payload::Shell {
+            user: "root".to_string(),
+            script: r#"echo "$PATH""#.to_string(),
+        },
+        Payload::Shell {
+            user: "root".to_string(),
+            script: "whoami".to_string(),
+        },
+        Payload::Shell {
+            user: "jeremy".to_string(),
+            script: "date".to_string(),
+        },
+        Payload::Shell {
+            user: "jeremy".to_string(),
+            script: r#"echo "$PATH""#.to_string(),
+        },
+        Payload::Shell {
+            user: "jeremy".to_string(),
+            script: "whoami".to_string(),
+        },
     ] {
         println!("run {}", it);
         println!("result {}", it.execute().unwrap());
